d607d37fe7e950fe7707309d08ab7bb3949979b6
frayxyz <55639860+frayxyz@users.noreply.github.com>
2024-01-28 20:11:19 +0000

    se agrega logica para consumo de api

--- app/src/index.js
+++ app/src/index.js
@@ -1,2 +1,19 @@
 console.log("ejecutandose logica javascript");
 let table = new DataTable('#myTable');
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log("ejecutandose funcion addeventlistener");
+    // Hacer la solicitud a la API
+    fetch('https://d0a771ny6f.execute-api.us-east-1.amazonaws.com/demo/getDataDemo')
+        .then(response =>{ 
+            console.log("respuesta servicio aws -> ",response);
+            return response.json();})
+        .then(data => {
+            // Agregar datos a la tabla DataTable
+            const table = $('#myTable').DataTable();
+            data.forEach(item => {
+                table.row.add([item.id, item.name, item.description]).draw();
+            });
+        })
+        .catch(error => console.error('Error al obtener datos:', error));
+});
